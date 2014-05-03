50b9c37a311f174094babb0e8397fa914be65ef1
olivieer <y.karasangabo@bg-rams.ac.at>
2014-05-03 17:13:03 -0400

    vendor: update qcom blobs
    Signed-off-by: olivieer <y.karasangabo@bg-rams.ac.at>

---
+++ msm7x27-common/vendor-blobs.mk
@@ -0,0 +1,20 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# OMX
+PRODUCT_COPY_FILES += \
+    vendor/htc/msm7x27-common/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
+    vendor/htc/msm7x27-common/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/htc/msm7x27-common/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
+    vendor/htc/msm7x27-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so
