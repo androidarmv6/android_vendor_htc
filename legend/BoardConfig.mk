175255ffb77786247f1c9adde97838d19d98ea86
olivieer <y.karasangabo@bg-rams.ac.at>
2014-05-06 16:32:02 -0400

    vendor: add legend blobs

---
+++ legend/BoardConfig.mk
@@ -0,0 +1,84 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+
+# inherit from common msm7x27
+-include device/htc/msm7x27-tiwlan-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/htc/legend/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := legend
+TARGET_PROVIDES_LIBRIL := device/htc/legend/prebuilt/system/lib/libhtc_ril.so
+TARGET_PROVIDES_INIT_TARGET_RC := true
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x12c00000
+
+BOARD_VENDOR_USE_AKMD := akm8973
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := legend
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/legend/bluetooth/include
+
+#$ adb shell cat /proc/mtd
+#dev:    size   erasesize  name
+#mtd0: 000a0000 00020000 "misc"
+#mtd1: 003e0000 00020000 "recovery"
+#mtd2: 00300000 00020000 "boot"
+#mtd3: 0f000000 00020000 "system"
+#mtd4: 02800000 00020000 "cache"
+#mtd5: 0b920000 00020000 "userdata"
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x003e0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00300000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0b920000
+
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+
+TARGET_PREBUILT_KERNEL := device/htc/legend/prebuilt/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Fix for Atmel touchscreens; trackball button
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+BOARD_USE_LEGACY_TRACKPAD := true
+
+
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WLAN_DEVICE := wl1271
+BOARD_SOFTAP_DEVICE := wl1271
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+WIFI_DRIVER_MODULE_PATH := /system/lib/modules/tiwlan_drv.ko
+WIFI_DRIVER_MODULE_NAME := tiwlan_drv
+WIFI_FIRMWARE_LOADER := wlan_loader
+WIFI_EXT_MODULE_PATH := /system/lib/modules/sdio.ko
+WIFI_EXT_MODULE_NAME := sdio
+
+
---
+++ legend/cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including mini_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := Legend
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/legend/legend.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := legend
+PRODUCT_NAME := cm_legend
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := Legend
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_VERSION_DEVICE_SPECIFIC := -UNOFFICIAL-ALPHA3
+CM_RELEASE := true
