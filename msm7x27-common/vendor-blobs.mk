448615083952dd720147d384a656845360148999
WinSuk <winsuk@winsuk.net>
2014-05-15 20:26:03 -0700

    msm7x27: Add comments to camera.msm7x27
    Change-Id: I83d0a9649ce525d39e5b31e8c605fbbd32b1207e

--- msm7x27-common/vendor-blobs.mk
+++ msm7x27-common/vendor-blobs.mk
@@ -17,5 +17,9 @@ PRODUCT_COPY_FILES += \
     vendor/htc/msm7x27-common/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
     vendor/htc/msm7x27-common/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
     vendor/htc/msm7x27-common/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
-    vendor/htc/msm7x27-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/htc/msm7x27-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so
+
+# Open source camera wrapper that can't be built with current GCC
+# https://github.com/SimplicityCM/android_device_htc_msm7x27-common/tree/cm-10.1/libcamera
+PRODUCT_COPY_FILES += \
     vendor/htc/msm7x27-common/proprietary/lib/hw/camera.msm7x27.so:system/lib/hw/camera.msm7x27.so
